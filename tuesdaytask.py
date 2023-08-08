5665545f46c665e1a12da99243c990ce7f5861fd
otto <otto@ubuntu.myguest.virtualbox.org>
2023-08-08 20:13:16 +0100

    Remove duplicate function

--- tuesdaytask.py
+++ tuesdaytask.py
@@ -15,42 +15,22 @@ def pendulum_simplified(t,x,g,length):
     """x is the state consisting of [postion, x1]"""
     return [x[1] ,(-g/length)*x[0]]
 
-
-
-def period_time_full(sol_full, sol_full_time):
+def period_time(sol, sol_time):
     points= [] 
     indexes = []
-    for i in range(len(sol_full)):
-        if i+2 > len(sol_full):
+    for i in range(len(sol)):
+        if i+2 > len(sol):
             break
-        if sol_full[i] < 0 and sol_full[i+1] >= 0 or sol_full[i] <= 0 and sol_full[i+1] > 0:
-            points.append(sol_full[i])
+        if sol[i] < 0 and sol[i+1] >= 0 or sol[i] <= 0 and sol[i+1] > 0:
+            points.append(sol[i])
             indexes.append(i)
             
         if len(points) >= 2:
             break
-    start_time = sol_full_time[indexes[0]]
-    end_time = sol_full_time[indexes[1]]
+    start_time = sol_time[indexes[0]]
+    end_time = sol_time[indexes[1]]
     total_time = end_time - start_time
     return total_time
-
-def period_time_simpl(sol_simpl, sol_simpl_time):
-    points= []
-    indexes = []
-    for i in range(len(sol_simpl)):
-        if i+2 > len(sol_simpl):
-            break
-        if sol_simpl[i] < 0 and sol_simpl[i+1] >= 0 or sol_simpl[i] <= 0 and sol_simpl[i+1] > 0:
-            points.append(sol_simpl[i])
-            indexes.append(i)
-
-        if len(points) >= 2:
-            break
-    start_time = sol_simpl_time[indexes[0]]
-    end_time = sol_simpl_time[indexes[1]]
-    total_time = end_time - start_time
-    return total_time
-
 
 t_vec =np.linspace(0,5,100)
 
@@ -66,9 +46,9 @@ def solver(ode_sys, g):
 sol_simpl = solver(pendulum_simplified, 9.81)
 sol_full = solver(pendulum, 9.81)
 
-print("The period of the oscillation from the full equation is: ", period_time_full(sol_full.y[0,:], sol_full.t), "seconds.")
+print("The period of the oscillation from the full equation is: ", period_time(sol_full.y[0,:], sol_full.t), "seconds.")
 
-print("The period of the oscillation from the simple equation is: ", period_time_simpl(sol_simpl.y[0,:], sol_simpl.t), "seconds.")
+print("The period of the oscillation from the simple equation is: ", period_time(sol_simpl.y[0,:], sol_simpl.t), "seconds.")
 
 
 #Plotting, create a 'figure' and an 'axes' within it
