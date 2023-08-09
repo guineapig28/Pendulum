ccb6a28a81e2fd35c572d49cd01a06f12447b128
otto <otto@ubuntu.myguest.virtualbox.org>
2023-08-09 08:46:32 +0100

    Graph showing period for different angles

--- tuesdaytask.py
+++ tuesdaytask.py
@@ -22,37 +22,54 @@ def period_time(sol, sol_time):
             break
         if sol[i] < 0 and sol[i+1] >= 0 or sol[i] <= 0 and sol[i+1] > 0:
             indexes.append(i)
-            
-    start_time = sol_time[indexes[0]]
-    end_time = sol_time[indexes[1]]
-    total_time = end_time - start_time
-    return total_time
+    try:        
+        start_time = sol_time[indexes[0]]
+        end_time = sol_time[indexes[1]]
+        total_time = end_time - start_time
+        return total_time
+    except:
+        return print("A period couldn't be calculated")
 
-t_vec =np.linspace(0,5,100)
+full_periods = []
+simpl_periods = []
+angle = []
 
-# If the starting angle is small, we don't care about the difference in the models, not significant
-starting_angle = 0.75
-length = 1 # pendulum length
+t_vec =np.linspace(0,5,1000)
 
-def solver(ode_sys, g):
-    """Solve the ODE system for different g and length values"""
-    return solve_ivp(ode_sys, [0,5], [starting_angle,0], args=(g,length), t_eval=t_vec)
+for i in range(1, 90):
+    # If the starting angle is small, we don't care about the difference in the models, not significant
+    starting_angle = i/57.2958
+    length = 1 # pendulum length
 
-# Get the different solutions for the full and simplified version
-sol_simpl = solver(pendulum_simplified, 9.81)
-sol_full = solver(pendulum, 9.81)
+    def solver(ode_sys, g):
+        """Solve the ODE system for different g and length values"""
+        return solve_ivp(ode_sys, [0,5], [starting_angle,0], args=(g,length), t_eval=t_vec)
 
-print("The period of the oscillation from the full equation is: ", period_time(sol_full.y[0,:], sol_full.t), "seconds.")
+    # Get the different solutions for the full and simplified version
+    sol_simpl = solver(pendulum_simplified, 9.81)
+    sol_full = solver(pendulum, 9.81)
 
-print("The period of the oscillation from the simple equation is: ", period_time(sol_simpl.y[0,:], sol_simpl.t), "seconds.")
+    full_periods.append(period_time(sol_full.y[0,:], sol_full.t))
+    simpl_periods.append(period_time(sol_simpl.y[0,:], sol_simpl.t))
+    angle.append(starting_angle)
 
 
-#Plotting, create a 'figure' and an 'axes' within it
 fig, ax = plt.subplots()
-# Add two lines to the axes
-ax.plot(t_vec, sol_simpl.y[0,:], label='Simplified')
-ax.plot(t_vec, sol_full.y[0,:], label='Full')
-ax.set_xlabel('Time [s]')
-ax.set_ylabel('Swing angle $\Theta$')
+ax.plot(angle, simpl_periods, label="Simplified")
+ax.plot(angle, full_periods, label="Full")
+ax.set_xlabel("Angle")
+ax.set_ylabel("Period")
 ax.legend()
 plt.show()
+
+#Plotting, create a 'figure' and an 'axes' within it
+#fig, ax = plt.subplots()
+# Add two lines to the axes
+#ax.plot(t_vec, sol_simpl.y[0,:], label='Simplified')
+#ax.plot(t_vec, sol_full.y[0,:], label='Full')
+#ax.set_xlabel('Time [s]')
+#ax.set_ylabel('Swing angle $\Theta$')
+#ax.legend()
+#plt.show()
+
+
