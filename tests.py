708a5209b3e92302795ca44f89a4b47581b9f68c
Adam Parrott <adam.parrott@ukaea.uk>
2023-08-09 14:05:43 +0100

    Add run_test.sh, combine tests

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from bayes_pendulum.tuesdaytask  import period_time
+from bayes_pendulum.tuesdaytask  import period_time, locate_root
 
 class TestMethods(unittest.TestCase):
 
@@ -10,3 +10,13 @@ class TestMethods(unittest.TestCase):
         sol_time = [0, 1, 2, 3]
         period = period_time(sol, sol_time)
         self.assertAlmostEqual(period, 2)
+
+    def test_locate_root(self):
+        """Tests the implementation of locate_root"""
+        # Generate example input from ODE solver
+        sol = [-1, 0, 1]
+        sol_time = [0, 1, 2]
+        index = 0
+        indexes =[1, 1, 1] 
+        root = locate_root(index, sol_time, sol, indexes)
+        self.assertAlmostEqual(root, 1)
