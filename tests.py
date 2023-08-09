fb6a9d7db962cd7b25a0108c0e56d50710333ce7
Adam Parrott <adam.parrott@ukaea.uk>
2023-08-09 11:19:29 +0100

    Change folder structure to allow for tests

---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+from .tuesdaytask import period_time
+
+class TestMethods(unittest.TestCase):
+
+    def test_period_time(self):
+        """Tests the implementation of period_time"""
+        # Generate example input from ODE solver
+        sol = [-1, 1, -1, 1]
+        sol_time = [0, 1, 2, 3]
+        period = period_time(sol, sol_time)
+        self.assertAlmostEqual(period, 3)
